2f4f5a509b2eaab73005619e02f6bf72a9e4ecf3
Om agarwal <black.falcon.x.69@gmail.com>
2025-07-05 14:10:11 +0530

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -58,7 +58,7 @@ console.log("Node Process Info:", process.pid, process.platform, process.version
 const allowedOrigins = [
   "http://localhost:5173",         // Dev Frontend
   "http://localhost:3000", 
-   "https://legal-aid1.vercel.app",        // Optional    // Your deployed frontend
+   "https://legal-aid-steel.vercel.app",        // Optional    // Your deployed frontend
 ];
 
 app.use(
